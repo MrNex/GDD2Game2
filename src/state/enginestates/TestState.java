992a786d5cef238268ea522573ccdda4cce2ef81
Alan Leeson <Alan@Alan-Leesons-MacBook.local>
2014-10-09 23:33:02 -0400

    Added a bouncy wall. Error in that the angle it defelct at is just opposite the angle it approached in

--- src/state/enginestates/TestState.java
+++ src/state/enginestates/TestState.java
@@ -9,7 +9,9 @@ import objects.GameObject;
 import objects.MovableGameObject;
 import state.objectstates.EnemyState;
 import state.objectstates.PlayerAimState;
+import state.objectstates.PlayerFlyState;
 import state.objectstates.PlayerState;
+import triggers.Trigger;
 
 public class TestState extends EngineState {
 
@@ -58,7 +60,9 @@ public class TestState extends EngineState {
 
 		addObj(testObj3);
 		
-		
+		//Bounce test obj
+		GameObject bounceObj1 = bounceyWall(800,200,35,100,new Vec(1,0));
+		addObj(bounceObj1);
 		
 	//Uncomment to see enemy behavior
 //		//Create an enemy
@@ -76,31 +80,70 @@ public class TestState extends EngineState {
 //		addObj(testObj4);
 	}
 	
+	/**
+	 * 
+	 */
 	public void createTestLevel(){
-		//Walls to bound you
+		//Walls to bound the screen
+		//Top boundary
 		GameObject upper = new GameObject(0,0,1200,20,new Vec(1,0));
 		upper.setShape(new Rectangle2D.Double());
 		upper.setVisible(true);
 		upper.setColor(Color.pink);
 		addObj(upper);
 		
+		//bottom boundary
 		GameObject bottom = new GameObject(0,685,1200,20,new Vec(1,0));
 		bottom.setShape(new Rectangle2D.Double());
 		bottom.setVisible(true);
 		bottom.setColor(Color.pink);
 		addObj(bottom);
 		
+		//left boundary
 		GameObject left = new GameObject(0,20,20,665,new Vec(1,0));
 		left.setShape(new Rectangle2D.Double());
 		left.setVisible(true);
 		left.setColor(Color.red);
 		addObj(left);
 		
+		//right boundary
 		GameObject right = new GameObject(1180,20,20,665,new Vec(1,0));
 		right.setShape(new Rectangle2D.Double());
 		right.setVisible(true);
 		right.setColor(Color.red);
 		addObj(right);
+	}
+	
+	/**
+	 * 
+	 * @param x position in world
+	 * @param y position in world
+	 * @param w width of rect to make
+	 * @param h height of rect to make
+	 * @param v vector for rotation
+	 * @return returns the object made
+	 */
+	public GameObject bounceyWall(int x, int y, int w, int h, Vec v){
+		GameObject obj = new GameObject(800,200,35,100,new Vec(1,0));
+		obj.setShape(new Rectangle2D.Double());
+		obj.setVisible(true);
+		obj.setColor(Color.green);
+		obj.setTriggerable(true);
+		obj.addTrigger(new Trigger(){
+
+			@Override
+			//triggered event
+			public void action(GameObject triggeredBy) {
+				// TODO Auto-generated method stub
+				triggeredBy.setState(new PlayerFlyState());
+				Vec force = triggeredBy.getForward();
+				force.normalize();
+				force.setMag(-1);
+				triggeredBy.setForward(force);
+			}
+			
+		});
+		return obj;
 	}
 
 
