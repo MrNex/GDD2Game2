aab2403bab4005b1bcde3764fb0ec24900f8c4bc
alanleeson <aml9227@rit.edu>
2014-10-16 20:30:06 -0400

    Rearranged some of TestState and began adding to the level

--- src/state/enginestates/TestState.java
+++ src/state/enginestates/TestState.java
@@ -53,111 +53,153 @@ public class TestState extends EngineState {
 		//Set camera to follow player
 		((CameraManager)Engine.currentInstance.getManager(Engine.Managers.CAMERAMANAGER)).setFollow(testObj.getPos());
 
-
-		//Captive Rats
-		GameObject captiveRat1 = new GameObject(400, 350, 20, 20, new Vec(1, 0));
-		captiveRat1.setShape(new Ellipse2D.Double());
-		captiveRat1.setVisible(true);
-		captiveRat1.setColor(Color.green);
-		captiveRat1.setState(new CaptiveRatState());
-		addObj(captiveRat1);
-
-
-		//Create bouncy walls
-		//Bounce test obj
-		GameObject bounceObj1 = bounceyWall(800,100,35,100,new Vec(1,0));
-		addObj(bounceObj1);
-		GameObject bounceObj2 = bounceyWall(800, 300, 35, 100, new Vec(1,0));
-		addObj(bounceObj2);
-		GameObject bounceObj3 = bounceyWall(800, 500, 35, 100, new Vec(1,0));
-		addObj(bounceObj3);
-
-		//Walls to bound the screen
-		//Top boundary
-		GameObject upper = new GameObject(0,0,1200,20,new Vec(1,0));
-		upper.setShape(new Rectangle2D.Double());
-		upper.setVisible(true);
-		//upper.setColor(Color.pink);
-		upper.setColor(Color.black);
-		addObj(upper);
-
-		//bottom boundary
-		GameObject bottom = new GameObject(0,685,1200,20,new Vec(1,0));
-		bottom.setShape(new Rectangle2D.Double());
-		bottom.setVisible(true);
-		//bottom.setColor(Color.pink);
-		bottom.setColor(Color.black);
-		addObj(bottom);
-
-		//left boundary
-		GameObject left = new GameObject(0,20,20,665,new Vec(1,0));
-		left.setShape(new Rectangle2D.Double());
-		left.setVisible(true);
-		//left.setColor(Color.red);
-		left.setColor(Color.black);
-		addObj(left);
-
-		//right boundary
-		GameObject right = new GameObject(1180,20,20,665,new Vec(1,0));
-		right.setShape(new Rectangle2D.Double());
-		right.setVisible(true);
-		//right.setColor(Color.red);
-		right.setColor(Color.black);
-		addObj(right);
-
-
-
-		GameObject midLine = new GameObject(700,335,70,30,new Vec(1,0));
-		midLine.setShape(new Rectangle2D.Double());
-		midLine.setVisible(true);
-		midLine.setColor(Color.black);
-		addObj(midLine);
-
-		GameObject buildingTop = new GameObject(400, 200, 100,100, new Vec(1,0));
-		buildingTop.setShape(new Rectangle2D.Double());
-		buildingTop.setVisible(true);
-		buildingTop.setColor(Color.gray);
-		addObj(buildingTop);
-
-		GameObject buildingBot = new GameObject(400, 400, 100,100, new Vec(1,0));
-		buildingBot.setShape(new Rectangle2D.Double());
-		buildingBot.setVisible(true);
-		buildingBot.setColor(Color.gray);
-		addObj(buildingBot);
-
-		GameObject buildingLongMid = new GameObject(550, 250, 50,200, new Vec(1,0));
-		buildingLongMid.setShape(new Rectangle2D.Double());
-		buildingLongMid.setVisible(true);
-		buildingLongMid.setColor(Color.gray);
-		addObj(buildingLongMid);
-
-		GameObject longTop = new GameObject(300, 20, 50,100, new Vec(1,0));
-		longTop.setShape(new Rectangle2D.Double());
-		longTop.setVisible(true);
-		longTop.setColor(Color.gray);
-		addObj(longTop);
-
-		GameObject longBot = new GameObject(300, 585, 50,100, new Vec(1,0));
-		longBot.setShape(new Rectangle2D.Double());
-		longBot.setVisible(true);
-		longBot.setColor(Color.gray);
-		addObj(longBot);
-
-		//hallway
-		GameObject hallTop = new GameObject(1000, 275, 200,50, new Vec(1,0));
-		hallTop.setShape(new Rectangle2D.Double());
-		hallTop.setVisible(true);
-		hallTop.setColor(Color.darkGray);
-		addObj(hallTop);
-
-		GameObject hallBot = new GameObject(1000, 375, 200,50, new Vec(1,0));
-		hallBot.setShape(new Rectangle2D.Double());
-		hallBot.setVisible(true);
-		hallBot.setColor(Color.darkGray);
-		addObj(hallBot);
-
+		firstSection();
+		secondSection();
+		thirdSection();
 	}
 
+	/*
+	 * sets up the scene for the first section
+	 */
+	public void firstSection(){
+		//Captive Rats
+				GameObject captiveRat1 = new GameObject(400, 350, 20, 20, new Vec(1, 0));
+				captiveRat1.setShape(new Ellipse2D.Double());
+				captiveRat1.setVisible(true);
+				captiveRat1.setColor(Color.green);
+				captiveRat1.setState(new CaptiveRatState());
+				addObj(captiveRat1);
+
+
+				//Create bouncy walls
+				//Bounce test obj
+				GameObject bounceObj1 = bounceyWall(800,100,35,100,new Vec(1,0));
+				addObj(bounceObj1);
+				GameObject bounceObj2 = bounceyWall(800, 300, 35, 100, new Vec(1,0));
+				addObj(bounceObj2);
+				GameObject bounceObj3 = bounceyWall(800, 500, 35, 100, new Vec(1,0));
+				addObj(bounceObj3);
+
+				//Walls to bound the screen
+				//Top boundary
+				GameObject upper = borderWall(0,0,1200,20,new Vec(1,0));
+				addObj(upper);
+
+				//bottom boundary
+				GameObject bottom = borderWall(0,685,1200,20,new Vec(1,0));
+				addObj(bottom);
+
+				//left boundary
+				GameObject left = borderWall(0,20,20,665,new Vec(1,0));
+				addObj(left);
+
+				//right boundary
+				GameObject right = borderWall(1180,20,20,300,new Vec(1,0));
+				addObj(right);
+				GameObject rightb = borderWall(1180,380,20,320,new Vec(1,0));
+				addObj(rightb);
+
+
+
+				GameObject midLine = new GameObject(700,335,70,30,new Vec(1,0));
+				midLine.setShape(new Rectangle2D.Double());
+				midLine.setVisible(true);
+				midLine.setColor(Color.black);
+				addObj(midLine);
+
+				GameObject buildingTop = new GameObject(400, 200, 100,100, new Vec(1,0));
+				buildingTop.setShape(new Rectangle2D.Double());
+				buildingTop.setVisible(true);
+				buildingTop.setColor(Color.gray);
+				addObj(buildingTop);
+
+				GameObject buildingBot = new GameObject(400, 400, 100,100, new Vec(1,0));
+				buildingBot.setShape(new Rectangle2D.Double());
+				buildingBot.setVisible(true);
+				buildingBot.setColor(Color.gray);
+				addObj(buildingBot);
+
+				GameObject buildingLongMid = new GameObject(550, 250, 50,200, new Vec(1,0));
+				buildingLongMid.setShape(new Rectangle2D.Double());
+				buildingLongMid.setVisible(true);
+				buildingLongMid.setColor(Color.gray);
+				addObj(buildingLongMid);
+
+				GameObject longTop = new GameObject(300, 20, 50,100, new Vec(1,0));
+				longTop.setShape(new Rectangle2D.Double());
+				longTop.setVisible(true);
+				longTop.setColor(Color.gray);
+				addObj(longTop);
+
+				GameObject longBot = new GameObject(300, 585, 50,100, new Vec(1,0));
+				longBot.setShape(new Rectangle2D.Double());
+				longBot.setVisible(true);
+				longBot.setColor(Color.gray);
+				addObj(longBot);
+
+				//hallway
+				GameObject hallTop = new GameObject(1000, 275, 200,50, new Vec(1,0));
+				hallTop.setShape(new Rectangle2D.Double());
+				hallTop.setVisible(true);
+				hallTop.setColor(Color.darkGray);
+				addObj(hallTop);
+
+				GameObject hallBot = new GameObject(1000, 375, 200,50, new Vec(1,0));
+				hallBot.setShape(new Rectangle2D.Double());
+				hallBot.setVisible(true);
+				hallBot.setColor(Color.darkGray);
+				addObj(hallBot);
+	}
+	
+	public void secondSection(){
+		//bottom
+		GameObject bottom = borderWall(1200,685,665,20,new Vec(1,0));
+		addObj(bottom);
+		
+		//right
+		GameObject right = borderWall(1845,-200,20,900,new Vec(1,0));
+		addObj(right);
+		
+		//top
+		GameObject top = borderWall(1200,-200,665,20,new Vec(1,0));
+		addObj(top);
+	}
+	
+	public void thirdSection(){
+		
+		//top border wall
+		GameObject top = borderWall(300,-200,900,10,new Vec(1,0));
+		addObj(top);
+		
+		//top bouncey object
+		GameObject bouncey1 = bounceyWall(300,-190,900,10,new Vec(1,0));
+		addObj(bouncey1);
+		
+		//bottom bouncey left
+		GameObject bouncey2 = bounceyWall(300,-1,350,10,new Vec(1,0));
+		addObj(bouncey2);
+		//bottom bouncey right
+		GameObject bouncey3 = bounceyWall(850,-1,350,10,new Vec(1,0));
+		addObj(bouncey3);
+	}
+	
+	/**
+	 * 
+	 * @param x position in world
+	 * @param y position in world
+	 * @param w width of rect to make
+	 * @param h height of rect to make
+	 * @param v vector for rotation
+	 * @return returns the object made
+	 */
+	public GameObject borderWall(int x, int y, int w, int h, Vec v){
+		GameObject obj = new GameObject(x,y,w,h,v);
+		obj.setShape(new Rectangle2D.Double());
+		obj.setVisible(true);
+		obj.setColor(Color.black);
+		return obj;
+	}
+	
 	/**
 	 * 
 	 * @param x position in world
